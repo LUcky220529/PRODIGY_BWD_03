484c92b30adacbe464b403d32884aee83f3073d4
LUcky220529 <lk5485029@gmail.com>
2025-06-26 13:21:49 +0530

    Update database.js

--- database.js
+++ database.js
@@ -10,30 +10,4 @@ const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, proces
 module.exports = { sequelize };
 
 
-// models/User.js
-const { DataTypes } = require('sequelize');
-const { sequelize } = require('../config/database');
 
-const User = sequelize.define('User', {
-  id: {
-    type: DataTypes.UUID,
-    defaultValue: DataTypes.UUIDV4,
-    primaryKey: true
-  },
-  name: DataTypes.STRING,
-  email: {
-    type: DataTypes.STRING,
-    unique: true,
-    validate: {
-      isEmail: true
-    }
-  },
-  password: DataTypes.STRING,
-  age: DataTypes.INTEGER,
-  role: {
-    type: DataTypes.STRING,
-    defaultValue: 'user'
-  }
-});
-
-module.exports = User;
